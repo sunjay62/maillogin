75cff57d51f77ae2c9d9175809715e08e41d2091
sunjay <jaya@tachyon.net.id>
2024-08-30 18:23:42 +0700

    update change password

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import logoPc24 from './assets/image/logopc24.png';
 import logoSaas from './assets/image/logosaas.png';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { Modal, Input, Button } from 'antd';
 
 function App() {
   const [email, setEmail] = useState('');
@@ -19,9 +20,13 @@ function App() {
   const [passwordError, setPasswordError] = useState('');
   const turnstileRef = useRef(null);
   const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const urlEndpoint = window.location.hostname;
-  console.log(urlEndpoint);
+  // console.log(urlEndpoint);
 
   useEffect(() => {
     if (!isScriptLoaded && !document.getElementById('turnstile-script')) {
@@ -78,15 +83,102 @@ function App() {
       formData.append('password', password);
       formData.append('cf-turnstile-response', turnstileResponse);
 
+      // for (let pair of formData.entries()) {
+      //   console.log(pair[0] + ': ' + pair[1]);
+      // }
+
       const apiEndpoint = `https://${urlEndpoint}/api/login`;
+      // console.log('API Endpoint for axios.post:', apiEndpoint);
 
       const responseData = await axios.post(apiEndpoint, formData);
+
+      // console.log(responseData);
 
       if (responseData.status === 200 && responseData.data.url) {
         window.location.href = responseData.data.url;
         toast.success('Login Successfully!', {
           position: 'top-right',
         });
+      } else if (responseData.status === 200 && responseData.data.change_pass) {
+        setIsModalVisible(true); // Show the modal if change_pass is true
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.status === 401) {
+        toast.error(error.response.data.messages, {
+          position: 'top-center',
+        });
+      } else if (error.response && error.response.status === 500) {
+        toast.error(error.message, {
+          position: 'top-center',
+        });
+      }
+    }
+  };
+
+  const validatePassword = (password) => {
+    const errors = [];
+    if (password.length < 8) {
+      errors.push('New Password must be at least 8 characters long.');
+    }
+    if (!/[A-Z]/.test(password)) {
+      errors.push('New Password must contain at least one uppercase letter.');
+    }
+    if (!/[0-9]/.test(password)) {
+      errors.push('New Password must contain at least one number.');
+    }
+    if (!/[!@#$%^]/.test(password)) {
+      errors.push('New Password must contain at least one special character (!@#$%^&*).');
+    }
+    return errors;
+  };
+
+  const handlePasswordChange = async () => {
+    // Check if new password and confirm password match
+    if (newPassword !== confirmPassword) {
+      toast.error('New password and confirm password do not match', {
+        position: 'top-center',
+      });
+      return;
+    }
+
+    // Validate new password
+    const validationErrors = validatePassword(newPassword);
+    if (validationErrors.length > 0) {
+      // Add numbering to each error message and include a <br /> tag for new line
+      const numberedErrors = validationErrors.map((error, index) => `${index + 1}. ${error}`).join('<br />');
+
+      toast.error(<div dangerouslySetInnerHTML={{ __html: numberedErrors }} />, {
+        position: 'top-center',
+      });
+      return;
+    }
+
+    try {
+      const formData = new FormData();
+      formData.append('email', email);
+      formData.append('old_password', password);
+      formData.append('new_password', confirmPassword);
+      formData.append('cf-turnstile-response', turnstileResponse);
+
+      // for (let pair of formData.entries()) {
+      //   console.log(pair[0] + ': ' + pair[1]);
+      // }
+
+      const apiChangePassword = `https://${urlEndpoint}/api/changepass`;
+      // console.log('API Endpoint for axios.post:', apiChangePassword);
+
+      const responseData = await axios.post(apiChangePassword, formData);
+
+      // console.log(responseData);
+
+      if (responseData.status === 200 && responseData.data.url) {
+        window.location.href = responseData.data.url;
+        toast.success('Login Successfully!', {
+          position: 'top-right',
+        });
+      } else if (responseData.status === 200 && responseData.data.change_pass) {
+        setIsModalVisible(true); // Show the modal if change_pass is true
       }
     } catch (error) {
       console.log(error);
@@ -106,6 +198,12 @@ function App() {
     if (event.key === 'Enter') {
       handleLogin();
     }
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    setNewPassword('');
+    setConfirmPassword('');
   };
 
   return (
@@ -188,9 +286,22 @@ function App() {
           <a href="https://remala.id/" target="_blank" className="linkRemala">
             Remala Group
           </a>{' '}
-          2024 All Right Reserved
+          All Rights Reserved
         </p>
       </div>
+
+      <Modal title="Change Password" className="modalContainer" open={isModalVisible} onOk={handlePasswordChange} onCancel={handleCancel}>
+        <div className="modalTop">
+          <p>Note: The new password must consist of at least 8 characters, including 1 uppercase letter, 1 number, and 1 special character (!@#$%^&*).</p>
+        </div>
+
+        <p>Current Password</p>
+        <Input.Password className="changePassword" placeholder="Current Password" value={password} onChange={(e) => setCurrentPassword(e.target.value)} />
+        <p>New Password</p>
+        <Input.Password className="changePassword" placeholder="New Password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+        <p>Confirm New Password</p>
+        <Input.Password className="changePassword" placeholder="Confirm New Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+      </Modal>
     </div>
   );
 }
